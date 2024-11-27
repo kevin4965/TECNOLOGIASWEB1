19e722b366c8a6b68175d52e916716b121157db3
JOSE QUEVIN <kevin4965@gmail.com>
2024-11-26 20:43:09 -0500

    ajs

--- app.js
+++ app.js
@@ -7,14 +7,17 @@ const clienteRoutes = require('./routes/clienteRoutes');
 const app = express();
 app.use(express.json());
 
-//conectar la base de datos
-mongoose.connect(process.env.MONGO_URL, {dbName: process.env.MONGO_DB_NAME,
-const db = mongoose.connection;
+// Conectar la base de datos
+mongoose.connect(process.env.MONGO_URL, {
+    dbName: process.env.MONGO_DB_NAME, 
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
 .then(() => console.log('Conectado a MongoDB'))
-.catch(error => console.error(error));
+.catch(error => console.error('Error al conectar a MongoDB:', error));
 
 app.use('/api/clientes', clienteRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
+
